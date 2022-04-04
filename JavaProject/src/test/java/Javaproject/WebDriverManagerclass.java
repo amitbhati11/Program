c89c0a51d5b973ad27fbbc5eaacdb22801f9f1b9
Admin_SRV <Admin_SRV@Admin_SRV-PC>
2022-04-04 22:13:58 +0530

    //webdrivermanager for all browser

---
+++ JavaProject/src/test/java/Javaproject/WebDriverManagerclass.java
@@ -0,0 +1,44 @@
+package Javaproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v97.browser.Browser;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.webdriver.WebDriverBrowser;
+
+public class WebDriverManagerclass {
+	static String browser;
+	static WebDriverManager drivermanager;
+	static WebDriver driver;
+	public static void browsersetup() {
+		browser ="chrome";
+
+
+	}
+	public static void browserconfiguration() {
+		if (browser.contains("chrome")) {
+
+			drivermanager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}
+		if(browser =="Firefox") {
+			drivermanager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}
+	}
+
+	public static void executetest() {
+		driver.get("www.google.com");
+		driver.quit();
+	}
+
+	public static void main(String[] args) {
+
+		browsersetup();
+		browserconfiguration();
+		executetest();
+	}
+
+}
